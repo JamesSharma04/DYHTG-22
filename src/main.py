09501f1769a5d15948d3507d79ca491dba83da68
James Sharma <65190976+JamesSharma04@users.noreply.github.com>
2022-10-14 20:54:40 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,5 +3,12 @@ import pandas as pd
 
 st.write("Hello WOrld")
 
-location = pd.read_csv("../data/location_data.csv")
-st.write(location)
+location_data = pd.read_csv("../data/location_data.csv")
+st.write(location_data)
+
+people_data = pd.read_csv("../data/people_data.csv")
+st.write(people_data)
+
+security_log_Data = pd.read_csv("../data/security_logs.csv")
+st.write(security_log_Data)
+
