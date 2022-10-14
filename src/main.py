b039e7183f5345b8ee2c986d57d52ddd883bfb97
Franciszek Sowul <2482997s@student.gla.ac.uk>
2022-10-14 21:52:49 +0100

    map with pydeck

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd 
+import pydeck as pdk
 
 st.write("Hello WOrld")
 
@@ -11,4 +12,30 @@ st.write(people_data)
 
 security_log_Data = pd.read_csv("../data/security_logs.csv")
 st.write(security_log_Data)
+
+coords = location_data.Geolocation.str.strip('{}').str.split(expand=True)
+coords.columns = ['lat', 'lon']
+
+coords = coords.astype(float)
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=55.873010,
+        longitude=-4.292536,
+        zoom=11,
+        pitch=60,
+    ),
+    layers=[
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=coords,
+            get_position='[lon, lat]',
+            get_color='[0, 0, 255, 255]',
+            get_radius=30,
+        ),
+    ],
+))
+
+
 
