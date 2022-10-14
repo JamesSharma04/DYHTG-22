0155261732d8f893e961e12f2fa5dfc02448d3d8
Robert Nimmo <robertnimmo26@live.co.uk>
2022-10-14 21:06:42 +0100

    Updated data path

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 import streamlit as st
-import pandas as pd 
+import pandas as pd
 
 st.write("Hello WOrld")
 
-location_data = pd.read_csv("../data/location_data.csv")
+location_data = pd.read_csv("data/location_data.csv")
 st.write(location_data)
 
-people_data = pd.read_csv("../data/people_data.csv")
+people_data = pd.read_csv("data/people_data.csv")
 st.write(people_data)
 
-security_log_Data = pd.read_csv("../data/security_logs.csv")
+security_log_Data = pd.read_csv("data/security_logs.csv")
 st.write(security_log_Data)
-
