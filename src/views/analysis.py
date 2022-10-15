5393d2b51fe386d7be20bb053d9f28a9bb1c0a10
Brandon Forrest <brandonf2002@live.co.uk>
2022-10-15 17:28:13 +0100

    Added the age hist to the analysis page

--- src/views/analysis.py
+++ src/views/analysis.py
@@ -1,4 +1,38 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import BytesIO
+
+location_data = pd.read_csv("data/location_data.csv")
+people_data = pd.read_csv("data/people_data.csv")
+security_log_Data = pd.read_csv("data/security_logs.csv")
+
+def age_histogram(bin_size):
+    data = people_data['Age'].values
+
+    st.write("People under 18")
+    st.write(str(people_data.loc[people_data['Age'] < 18]['Name'].values)
+                 .replace('[\'', '')
+                 .replace('\']', '')
+                 .replace('\' \'', ', ')
+                 )
+
+    st.write("People over 30")
+    st.write(str(people_data.loc[people_data['Age'] > 30]['Name'].values)
+                 .replace('[\'', '')
+                 .replace('\']', '')
+                 .replace('\' \'', ', ')
+                 )
+
+    fig, ax = plt.subplots(figsize=(7, 7))
+    ax.hist(data, bins=bin_size, color='c', alpha=0.65)
+    ax.vlines(18, 0, 30, color='k', label="18")
+    ax.vlines(30, 0, 30, color='k', label="30")
+
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    st.image(buf)
 
 def load_view():    
     st.title('Analysis Page')
+    age_histogram(20)
