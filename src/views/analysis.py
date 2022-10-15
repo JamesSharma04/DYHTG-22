c6d24a58e88d4545b33af5ea1564e13ef2215400
Robert Nimmo <robertnimmo26@live.co.uk>
2022-10-15 21:11:32 +0100

    Presenting summary based on all remaining students

--- src/views/analysis.py
+++ src/views/analysis.py
@@ -7,23 +7,25 @@ location_data = pd.read_csv("data/location_data.csv")
 people_data = pd.read_csv("data/people_data.csv")
 security_log_Data = pd.read_csv("data/security_logs.csv")
 
+
 def age_histogram(bin_size):
     data = people_data['Age'].values
 
-    st.write("People under 18")
+    st.subheader("People under 18")
     st.write(str(people_data.loc[people_data['Age'] < 18]['Name'].values)
-                 .replace('[\'', '')
-                 .replace('\']', '')
-                 .replace('\' \'', ', ')
-                 )
+             .replace('[\'', '')
+             .replace('\']', '')
+             .replace('\' \'', ', ')
+             )
 
-    st.write("People over 30")
+    st.subheader("People over 30")
     st.write(str(people_data.loc[people_data['Age'] > 30]['Name'].values)
-                 .replace('[\'', '')
-                 .replace('\']', '')
-                 .replace('\' \'', ', ')
-                 )
+             .replace('[\'', '')
+             .replace('\']', '')
+             .replace('\' \'', ', ')
+             )
 
+    st.subheader("Histogram of ages")
     fig, ax = plt.subplots(figsize=(7, 7))
     ax.hist(data, bins=bin_size, color='c', alpha=0.65)
     ax.vlines(18, 0, 30, color='k', label="18")
@@ -33,6 +35,7 @@ def age_histogram(bin_size):
     fig.savefig(buf, format="png")
     st.image(buf)
 
-def load_view():    
+
+def load_view():
     st.title('Analysis Page')
     age_histogram(20)
